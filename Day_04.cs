e4e70ff5c1eab50795bab20c0f5b438287e43ae9
Siva Prakash <prakashdeveloper@outlook.in>
2023-04-18 12:28:21 +0530

    Added Day_04.cs & Day_05.cs files

---
+++ Day_04.cs
@@ -0,0 +1,40 @@
+using System;
+
+class Person {
+    public int age;     
+    public Person(int initialAge) {
+        if (initialAge < 0) {
+            Console.WriteLine("Age is not valid, setting age to 0.");
+            this.age = 0;
+        } else {
+            this.age = initialAge;
+        }
+     }
+     public void amIOld() {
+        if (age < 13) {
+            Console.WriteLine("You are young.");
+        } else if (age >= 13 && age < 18) {
+            Console.WriteLine("You are a teenager.");
+        } else {
+            Console.WriteLine("You are old.");
+        } 
+     }
+
+     public void yearPasses() {
+        this.age += 1;
+     }
+
+    static void Main(String[] args) {
+        int T = int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age = int.Parse(Console.In.ReadLine());
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            Console.WriteLine();
+        }
+    }
+}
