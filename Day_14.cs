06c7c4ffdb6fb7f21ebf9b78f38f9439b4afb6ff
Siva Prakash <prakashdeveloper@outlook.in>
2023-04-23 09:59:04 +0530

    Added Day_14.cs & Day_15.cs files

---
+++ Day_14.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+    public Difference(int[] integers) {
+        elements = integers;    
+    }
+    
+    public void computeDifference() {
+        var maxDeger = elements.Max();
+        var minDeger = elements.Min();
+        maximumDifference = Math.Abs(maxDeger - minDeger);
+    }
+}
+
+class Solution {
+    static void Main(string[] args) {
+        Convert.ToInt32(Console.ReadLine());        
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        Difference d = new Difference(a);
+        d.computeDifference();
+        Console.Write(d.maximumDifference);
+    }
+}
