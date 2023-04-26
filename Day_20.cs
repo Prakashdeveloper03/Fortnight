4af6220801ac438c6961cf6b9844d4fa2eda349d
Siva Prakash <prakashdeveloper@outlook.in>
2023-04-26 06:40:40 +0530

    Added Day_20.cs & Day_21.cs files

---
+++ Day_20.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Solution {
+    public static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+        int numberOfSwaps = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - 1;  j++) {
+                if(a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+            
+            if(numberOfSwaps == 0) {
+                break;
+            }
+        }
+        Console.WriteLine($"Array is sorted in {numberOfSwaps} swaps.");
+        Console.WriteLine($"First Element: {a[0]}");
+        Console.WriteLine($"Last Element: {a[a.Count - 1]}");
+    }
+}
---
+++ Day_21.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Printer
+{
+	public static void PrintArray<T>(T[] array) {
+        foreach(var item in array) {
+            Console.WriteLine(item);
+        }
+    }
+
+    static void Main(string[] args) {
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+			stringArray[i] = Console.ReadLine();
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
